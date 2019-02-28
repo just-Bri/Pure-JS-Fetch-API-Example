3a1bf3cf3593e77fc4d47f5e2d84f421a9270e39
Bridger <bridgerrhammond@gmail.com>
2019-02-28 14:38:22 -0600

    Converted to arrow functions

--- app.js
+++ app.js
@@ -7,52 +7,35 @@ document.getElementById("button3").addEventListener("click", getExternal);
 // Get local text file data
 function getText() {
   fetch("test.txt")
-    .then(function(res) {
-      return res.text();
+    .then(res => res.text())
+    .then(data => {
+      console.log(data), (document.getElementById("output").innerHTML = data);
     })
-    .then(function(data) {
-      console.log(data);
-      document.getElementById("output").innerHTML = data;
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 // Get local json data
 function getJson() {
   fetch("posts.json")
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = "";
-      data.forEach(function(post) {
-        output += `<li>${post.title}</li>`;
-      });
+      data.forEach(post => (output += `<li>${post.title}</li>`));
       document.getElementById("output").innerHTML = output;
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 // Get from external API
 function getExternal() {
   fetch("https://api.github.com/users")
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = "";
-      data.forEach(function(user) {
-        output += `<li>${user.login}</li>`;
-      });
+      data.forEach(user => (output += `<li>${user.login}</li>`));
       document.getElementById("output").innerHTML = output;
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
