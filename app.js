e5a3e7d9c216abb14a9c23055336475b119f2277
Bridger <bridgerrhammond@gmail.com>
2019-03-01 13:03:48 -0600

    Converted to fetch with async await

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-document.getElementById("button1").addEventListener("click", getText);
+const http = new EasyHTTP();
 
-document.getElementById("button2").addEventListener("click", getJson);
+// User Data
+const data = {
+  name: "John Doe",
+  username: "johndoe",
+  email: "jdoe@gmail.com"
+};
 
-document.getElementById("button3").addEventListener("click", getExternal);
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Get local text file data
-function getText() {
-  fetch("test.txt")
-    .then(res => res.text())
-    .then(data => {
-      console.log(data), (document.getElementById("output").innerHTML = data);
-    })
-    .catch(err => console.log(err));
-}
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// Get local json data
-function getJson() {
-  fetch("posts.json")
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      let output = "";
-      data.forEach(post => (output += `<li>${post.title}</li>`));
-      document.getElementById("output").innerHTML = output;
-    })
-    .catch(err => console.log(err));
-}
-
-// Get from external API
-function getExternal() {
-  fetch("https://api.github.com/users")
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      let output = "";
-      data.forEach(user => (output += `<li>${user.login}</li>`));
-      document.getElementById("output").innerHTML = output;
-    })
-    .catch(err => console.log(err));
-}
+// Delete User
+http
+  .delete("https://jsonplaceholder.typicode.com/users/2")
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
